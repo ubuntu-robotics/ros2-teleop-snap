e40bb9665264ef39b20575b17ff2e11c934bdd15
Giuseppe Barbieri <giuseppe.barbieri@canonical.com>
2024-04-18 18:21:05 +0200

    fix connect hook to start core daemon automatically, add parameters for topics

--- snap/local/key_launcher.sh
+++ snap/local/key_launcher.sh
@@ -2,4 +2,4 @@
 
 ROS2_TELEOP_KEY_VEL_TOPIC="$(snapctl get key-vel)"
 
-ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args --remap cmd_vel:=$ROS2_TELEOP_KEY_VEL_TOPIC
+ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args --remap cmd_vel:=${ROS2_TELEOP_KEY_VEL_TOPIC}
--- snap/local/topic_tools_mux.sh
+++ snap/local/topic_tools_mux.sh
@@ -2,8 +2,8 @@
 
 ROS2_TELEOP_KEY_VEL_TOPIC="$(snapctl get key-vel)"
 ROS2_TELEOP_JOY_VEL_TOPIC="$(snapctl get joy-vel)"
-ROS2_TELEOP_CMD_VEL_TOPIC="$(snapctl set cmd-vel)"
+ROS2_TELEOP_CMD_VEL_TOPIC="$(snapctl get cmd-vel)"
 ROS2_TELEOP_NAV_VEL_TOPIC="$(snapctl get nav-vel)"
 ROS2_TELEOP_WEB_VEL_TOPIC="$(snapctl get web-vel)"
 
-ros2 run topic_tools mux "${ROS2_TELEOP_CMD_VEL_TOPIC}" "${ROS2_TELEOP_KEY_VEL_TOPIC}" "${ROS2_TELEOP_NAV_VEL_TOPIC}" "{$ROS2_TELEOP_JOY_VEL_TOPIC}" "{$ROS2_TELEOP_WEB_VEL_TOPIC}"
+ros2 run topic_tools mux "${ROS2_TELEOP_CMD_VEL_TOPIC}" "${ROS2_TELEOP_KEY_VEL_TOPIC}" "${ROS2_TELEOP_NAV_VEL_TOPIC}" "${ROS2_TELEOP_JOY_VEL_TOPIC}" "${ROS2_TELEOP_WEB_VEL_TOPIC}"
